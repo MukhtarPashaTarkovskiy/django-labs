b100c183b663e5cb4a9cd1fd341d0c908a702a7d
Mukhtar-Pasha <kasp2222@mail.ru>
2026-03-08 07:38:42 +0300

    Move secret key to env

--- admin_learning/admin_learning/settings.py
+++ admin_learning/admin_learning/settings.py
@@ -11,19 +11,20 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#ppyhx8!lfd@c49vub@+%_#l64(te@ecpskzp964x-cd_%8rnm'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = []
 
